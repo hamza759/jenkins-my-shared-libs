3a1c0c4cd12d121cd0ff80489fdc6e704e668e35
hamza <69013863+hamza759@users.noreply.github.com>
2025-09-22 13:45:16 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,11 +1,11 @@
-def call (String credId String imageName) {
+def call (String credId, String imageName) {
      withCredentials([usernamePassword(
                     credentialsId:"${credId}",    // .... here i used demohub  .....
                     passwordVariable:"dockerHubPass",
                     usernameVariable:"dockerHubUser"
                     )]){
                 sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker image tag "${imageName}" ${env.dockerHubUser}/${imageName} "  // here the the image name is two-tier-flask-app
+                sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName} "  // here the the image name is two-tier-flask-app
                 sh "docker push ${env.dockerHubUser}/${imageName}"
             }
 }
