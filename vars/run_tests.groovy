42a89bfeeae4cf7462a5dca221448cada610544d
hamza <69013863+hamza759@users.noreply.github.com>
2025-11-05 16:21:27 +0500

    Update run_tests.groovy

--- vars/run_tests.groovy
+++ vars/run_tests.groovy
@@ -2,7 +2,7 @@ def call() {
     echo "Running unit tests..."
     
     try {
-        sh "mvn clean test"
+        sh "mvn clean install -DskipTests"
         echo "Unit tests completed successfully"
     } catch (Exception e) {
         error "Unit tests failed! Build stopped."
